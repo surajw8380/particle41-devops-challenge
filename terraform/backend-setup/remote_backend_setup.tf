034cb413b18c368f1c8d27694221175cf015b331
Suraj Waghamare <surajnm.waghmare@gmail.com>
2025-06-07 17:43:33 +0530

    Create remote_backend_setup.tf

---
+++ terraform/backend-setup/remote_backend_setup.tf
@@ -0,0 +1,37 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "simpletime-terraform-state-bucket"
+  force_destroy = true
+
+  tags = {
+    Name        = "Terraform State Bucket"
+    Environment = "dev"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "simpletime-terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "Terraform Lock Table"
+    Environment = "dev"
+  }
+}
